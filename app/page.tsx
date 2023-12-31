a5ecd2c78ede40c82e1c2fe8c8d8b022382352b6
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-12-31 15:52:46 +0530

    feat: home page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
+import { Poppins } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div className="space-y-6">
-        <h1 className="text-6xl font-semibold drop-shadow-md text-white">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold drop-shadow-md text-white",
+            font.className
+          )}
+        >
           🔐 Auth
         </h1>
         <p className="text-lg text-white">A simple Authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
       </div>
     </main>
   );
